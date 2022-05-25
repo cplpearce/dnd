6d2ef3ff6c560793114730cd08fa8c6b281f18d2
Clinton Pearce <clint.pearce@rocketmail.com>
2022-05-25 01:03:13 -0400

    Update ignore, rename files, add logic to class

--- app.py
+++
@@ -1,7 +0,0 @@
-"""This is the base script to launch the game"""
-
-from .scripts.classes.entity import Entity
-
-max_the_hero = Entity("Max", 100, 10)
-
-max_the_hero.attack(Entity("John", 100, 10))
---
+++ utils/classes/entity.py
@@ -0,0 +1,56 @@
+# pylint: disable=invalid-name
+"""This class file contains the Entity class"""
+import random
+
+
+class Entity:
+    """This class contains the Entity class"""
+
+    def __init__(self, name, hitpoints, attack_bonus, damage_die, damage_bonus):
+        """Initialize the base entity"""
+        self.name = name
+        self.ac = 10
+        self.hitpoints = hitpoints
+        self.attack_bonus = attack_bonus
+        self.damage_die = damage_die
+        self.damage_bonus = damage_bonus
+
+    def attack(self, target):
+        """Attack another entity and subtract damage from target's hitpoints"""
+
+        # get the result of the die roll
+        hit_result = random.randint(1, 20) + self.attack_bonus
+
+        # if the result is higher than the target's AC, then we hit
+        print(f"{self.name} attacks {target.name} with an attack of {hit_result}...")
+        if hit_result > target.ac:
+
+            # now we calc the damage
+            damage_result = random.randint(1, self.damage_die) + self.damage_bonus
+
+            print(f"And hits! For {damage_result} damage!")
+
+            # reduce enemy hitpoints
+            target.hitpoints -= damage_result
+
+        else:
+            print("And misses!")
+            print()
+
+    def is_alive(self):
+        """Is the entity alive?"""
+        return self.hitpoints > 0
+
+    # this @property means we can GET something, and we alias hitpoints to hp
+    @property
+    def hp(self):
+        """Get the entity hitpoints"""
+        return self.hitpoints
+
+    # this @setter property decorator means we can SET its hp using this alias
+    @hp.setter
+    def hp(self, damage):
+        self.hitpoints += damage
+
+    def __str__(self):
+        return f"{self.name} has {self.hitpoints} hitpoints"
